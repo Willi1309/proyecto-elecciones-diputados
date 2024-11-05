6d3ab3e6d1249f0ddba7ea1b40b7d01f5ebb6b45
Arnaldo Flores <arnaldoflores58@gmail.com>
2024-11-05 12:21:37 -0400

    Se agrego la libreria pila al git

---
+++ Libreria/Pila.h
@@ -0,0 +1,33 @@
+#ifndef LIBRERIA_PILA_H_
+#define LIBRERIA_PILA_H_
+#include <string>
+#include <cstdlib>
+#include <iostream>
+
+class NodoPila {
+public:
+    string info;
+    NodoPila *sig;
+    NodoPila(std::string str){
+    	info = str;
+    	sig = NULL;
+    }
+};
+
+class Pila{
+	private:
+	NodoPila* tope;
+	public:
+		Pila(){
+			tope = NULL;
+		}
+		void insertarPila(std::string str){
+			NodoPila* nuevoNodo = new NodoPila(str);
+			tope = nuevoNodo;
+		}
+		std::string obtenerPila(){
+			return tope->info;
+		}
+};
+
+#endif /* LIBRERIA_PILA_H_ */
